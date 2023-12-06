b8fefacb8a8c107a65e182af1483298c9d33779c
chandrasehkhar <chandrasehkhar18@gmail.com>
2023-12-06 23:26:23 +0000

    added health endpoint

--- app.py
+++ app.py
@@ -2,11 +2,26 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+health_status = False
 
 @app.route('/hello')
-def index():
+def hello():
+    global health_status
+    health_status = not health_status
     return jsonify({'message': 'hello world'})
 
 
+@app.route('/health')
+def health():
+    if health_status:
+        resp = jsonify(health="Success")
+        resp.status_code = 200
+    else:
+        resp = jsonify(health="unhealthy")
+        resp.status_code = 500
+
+    return resp
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True)
